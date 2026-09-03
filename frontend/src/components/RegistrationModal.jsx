4151507103bb4835ad4023ea09f81dc36f06b73e
giriYT26 <srikalgiri@gmail.com>
2026-09-03 13:00:28 +0530

    fix: remove VIEW COORDINATORS button, add registration modal mobile layout

--- frontend/src/components/RegistrationModal.jsx
+++ frontend/src/components/RegistrationModal.jsx
@@ -249,7 +249,7 @@ export default function RegistrationModal({ events, registrationOpen, initialEve
               </h2>
             </div>
             <button ref={closeButtonRef} className="reg-close" onClick={onClose} aria-label="Close registration">
-              <span>CLOSE</span> [X]
+              <span className="reg-close-label">CLOSE</span> [X]
             </button>
           </header>
 
